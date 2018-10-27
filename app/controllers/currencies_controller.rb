4ca42dff4a297f825668cf20ceaf5c8f72c267d8
ManishArora93 <manisharora454@gmail.com>
2018-10-28 00:17:35 +0530

    Viewing the currencies

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -1,6 +1,6 @@
 class CurrenciesController < ApplicationController
+  caches_action :index, expires_in: 5.minutes
   def index
-  	@currencies = CoinMarket::Ticker.all[0..10]
-  	p @currencies
+  	@currencies = CoinMarket::Ticker.all[0..9]
   end
 end
